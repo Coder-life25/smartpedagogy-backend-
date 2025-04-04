d017e4f07004c6b3f8c5fd2a9958e52c0ab1732a
Coder-life25 <prajeetshahlac@gmail.com>
2025-04-05 01:41:08 +0545

    fix cors issue

--- server.js
+++ server.js
@@ -17,7 +17,21 @@ const teacherFeedbackInsightRoute = require("./routes/teacherFeedbackInsightRout
 
 app.use(express.json({ limit: "100mb" }));
 app.use(bodyParser.urlencoded({ limit: "100mb", extended: true }));
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(",") || [
+  "https://smart-pedagogy.vercel.app",
+];
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 
 app.use("/", authRouter);
